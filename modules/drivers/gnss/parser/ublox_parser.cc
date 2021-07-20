04eb565294fe77ae98d5c9b300188f9b1a6eb974
Alex <alexzhou90@gmail.com>
2021-07-20 16:41:56 -0400

    ublox testing

--- modules/drivers/gnss/parser/ublox_parser.cc
+++ modules/drivers/gnss/parser/ublox_parser.cc
@@ -26,16 +26,73 @@ Parser* Parser::CreateUblox(const config::Config& config) {
     return new UbloxParser();
 }
 
-
 // UbloxParser::UbloxParser() {
     
 // }
 
 Parser::MessageType UbloxParser::GetMessage(MessagePtr *message_ptr) {
-//   if (data_ == nullptr) {
-//     return MessageType::NONE;
-//   }
+  if (data_ == nullptr) {
+    return MessageType::NONE;
+  }
+  while (data_ < data_end_) {
+    if (buffer_.empty()) {
+      if (*data_ == novatel::SYNC_0) {
+        buffer_.push_back(*data_);
+      }
+      ++data_;
+    } else if (buffer_.size() == 1) {  // Looking for SYNC1
+      if (*data_ == novatel::SYNC_1) {
+        buffer_.push_back(*data_++);
+      } else {
+        buffer_.clear();
+      }
+    } else if (buffer_.size() == 2) {  // Looking for SYNC2
+      switch (*data_) {
+        case novatel::SYNC_2_LONG_HEADER:
+          buffer_.push_back(*data_++);
+          header_length_ = sizeof(novatel::LongHeader);
+          break;
+        case novatel::SYNC_2_SHORT_HEADER:
+          buffer_.push_back(*data_++);
+          header_length_ = sizeof(novatel::ShortHeader);
+          break;
+        default:
+          buffer_.clear();
+      }
+    } else if (header_length_ > 0) {  // Working on header.
+      if (buffer_.size() < header_length_) {
+        buffer_.push_back(*data_++);
+      } else {
+        if (header_length_ == sizeof(novatel::LongHeader)) {
+          total_length_ = header_length_ + novatel::CRC_LENGTH +
+                          reinterpret_cast<novatel::LongHeader*>(buffer_.data())
+                              ->message_length;
+        } else if (header_length_ == sizeof(novatel::ShortHeader)) {
+          total_length_ =
+              header_length_ + novatel::CRC_LENGTH +
+              reinterpret_cast<novatel::ShortHeader*>(buffer_.data())
+                  ->message_length;
+        } else {
+          AERROR << "Incorrect header_length_. Should never reach here.";
+          buffer_.clear();
+        }
+        header_length_ = 0;
+      }
+    } else if (total_length_ > 0) {
+      if (buffer_.size() < total_length_) {  // Working on body.
+        buffer_.push_back(*data_++);
+        continue;
+      }
+      MessageType type = PrepareMessage(message_ptr);
+      buffer_.clear();
+      total_length_ = 0;
+      if (type != MessageType::NONE) {
+        return type;
+      }
+    }
+  }
   return MessageType::NONE;
+
 }
 
 bool UbloxParser::verify_checksum() { return true; }
